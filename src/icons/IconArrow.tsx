6b7f1ced1ef3420c567bc2af3b1083301bedeb54
codehans <94654388+codehans@users.noreply.github.com>
2022-12-27 12:26:30 +0000

    IconArrow

--- src/icons/IconArrow.tsx
+++ src/icons/IconArrow.tsx
@@ -1,21 +1,19 @@
-export function IconArrow({
-  className = "",
-}: {
-  className?: string;
-}) {
-  return (
-    <svg
-      width="16px"
-      height="16px"
-      viewBox="0 0 16 16"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}>
-      <path
-        d="M7.535 11.951l-6.342-6.44a.657.657 0 010-.93l.388-.388a.657.657 0 01.93 0L8 9.786l5.49-5.588a.657.657 0 01.93 0l.387.388a.657.657 0 010 .93l-6.342 6.44a.662.662 0 01-.93-.005z"
-        fill="currentColor"
-        fillRule="nonzero"
-      />
-    </svg>
-  );
-}
+import { FC } from "react";
+
+export const IconArrow: FC<JSX.IntrinsicElements["svg"]> = (
+  props
+) => (
+  <svg
+    width="16px"
+    height="16px"
+    viewBox="0 0 16 16"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}>
+    <path
+      d="M7.535 11.951l-6.342-6.44a.657.657 0 010-.93l.388-.388a.657.657 0 01.93 0L8 9.786l5.49-5.588a.657.657 0 01.93 0l.387.388a.657.657 0 010 .93l-6.342 6.44a.662.662 0 01-.93-.005z"
+      fill="currentColor"
+      fillRule="nonzero"
+    />
+  </svg>
+);
