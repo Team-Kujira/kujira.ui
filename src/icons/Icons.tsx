f3659212cd9d6ee0de295d92c795b82c12f3343b
slangdog <hello@brettmeyer.net>
2024-03-15 16:42:30 +0000

    icons

--- src/icons/Icons.tsx
+++ src/icons/Icons.tsx
@@ -54,6 +54,18 @@ export const ChartUp = ({
   </svg>
 );
 
+export const Check = ({ className = "" }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 448 512"
+    className={className}>
+    <path
+      d="M441 103c9.4 9.4 9.4 24.6 0 33.9L177 401c-9.4 9.4-24.6 9.4-33.9 0L7 265c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l119 119L407 103c9.4-9.4 24.6-9.4 33.9 0z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
 export const Copy = ({ className = "" }: { className?: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
