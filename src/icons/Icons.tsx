31b58cea1f6832a180269df0ebc7d62827d08ad5
slangdog <hello@brettmeyer.net>
2024-05-08 14:16:37 +0100

    icons

--- src/icons/Icons.tsx
+++ src/icons/Icons.tsx
@@ -14,6 +14,38 @@ export const Chain = ({ className = "" }: { className?: string }) => (
 
 // Light
 
+export const AngleDown = ({
+  className = "",
+}: {
+  className?: string;
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    className={className}>
+    <path
+      d="M267.3 395.3c-6.2 6.2-16.4 6.2-22.6 0l-192-192c-6.2-6.2-6.2-16.4 0-22.6s16.4-6.2 22.6 0L256 361.4 436.7 180.7c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6l-192 192z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const AngleUp = ({
+  className = "",
+}: {
+  className?: string;
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    className={className}>
+    <path
+      d="M244.7 116.7c6.2-6.2 16.4-6.2 22.6 0l192 192c6.2 6.2 6.2 16.4 0 22.6s-16.4 6.2-22.6 0L256 150.6 75.3 331.3c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l192-192z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
 export const Book = ({ className = "" }: { className?: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
