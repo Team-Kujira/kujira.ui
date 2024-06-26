b5dd721db02f80e06d4501e60d384ac34d67c5b1
slangdog <hello@brettmeyer.net>
2024-06-26 21:53:06 +0100

    angle right

--- src/icons/Icons.tsx
+++ src/icons/Icons.tsx
@@ -62,6 +62,22 @@ export const AngleLeft = ({
   </svg>
 );
 
+export const AngleRight = ({
+  className = "",
+}: {
+  className?: string;
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 320 512"
+    className={className}>
+    <path
+      d="M299.3 244.7c6.2 6.2 6.2 16.4 0 22.6l-192 192c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6L265.4 256 84.7 75.3c-6.2-6.2-6.2-16.4 0-22.6s16.4-6.2 22.6 0l192 192z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
 export const AngleUp = ({
   className = "",
 }: {
