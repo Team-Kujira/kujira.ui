2a8ab308e367840bd7dd5bfe6d2a308d416e837a
slangdog <hello@brettmeyer.net>
2024-07-03 07:59:21 +0100

    position abs

--- src/components/Popout.tsx
+++ src/components/Popout.tsx
@@ -3,6 +3,17 @@ import { FC, useEffect, useRef, useState } from "react";
 import { GradientButton } from "kujira-core/src/components";
 import { AngleLeft, Ellipsis } from "kujira.ui/src/icons/Icons";
 
+function getParentOffset(el: HTMLElement) {
+  const rect = el.getBoundingClientRect();
+  const p = el.closest(".relative")?.getBoundingClientRect();
+  return {
+    left: rect.left - (p?.left || 0),
+    top: rect.top - (p?.top || 0),
+    right: (p?.right || 0) - rect.right,
+    bottom: (p?.bottom || 0) - rect.bottom,
+  };
+}
+
 export const Popout: FC<{
   children?: React.ReactNode;
   className?: string;
@@ -14,6 +25,29 @@ export const Popout: FC<{
   useEffect(() => {
     if (open) {
       document.addEventListener("mousedown", handleClickOutside);
+      const nav = node.current?.querySelector("nav");
+      if (node.current && nav) {
+        const p = getParentOffset(node?.current);
+        nav.style.bottom = p.bottom + "px";
+        nav.style.right =
+          p.right + node.current.offsetWidth + 4 + "px";
+      }
+      /* const nav = node.current?.querySelector("nav");
+      if (nav) {
+        const p = nav.offsetParent;
+        if (p) {
+          console.log(getComputedStyle(p).position);
+        }
+      } */
+      //const rect = node.current?.getBoundingClientRect();
+
+      /* if (nav) {
+        const top = getTopOffset(nav);
+        console.log(top);
+        nav.style.bottom = top + "px";
+        // nav.style.right = rect?.left + "px";
+      } */
+
       /* const ref = drop.current;
       if (ref) {
         const box = ref.getBoundingClientRect();
